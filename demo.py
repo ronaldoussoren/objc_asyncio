2943beed5c229c9fb0d2e6cb66e8ef2f05bab131
Ronald Oussoren <ronaldoussoren@mac.com>
2020-03-27 15:26:39 +0100

    Tweak implementation, basic async functions now work
    Note that 'asyncio.sleep' does not yet work...

--- demo.py
+++ demo.py
@@ -1,8 +1,40 @@
+import asyncio
+from asyncio import futures
+
 from objc_asyncio import _loop as loop
 
+
+async def sleep(delay, result=None):
+    future = el.create_future()
+    h = el.call_later(delay, futures._set_result_unless_cancelled, future, result)
+    try:
+        value = await future
+        return value
+    finally:
+        h.cancel()
+
+
+async def printer():
+    print("******* start")
+    await sleep(1)
+    print("******* middle")
+    await sleep(1)
+    print("******* end")
+
+
+async def main():
+    print("******* hello")
+    await printer()
+    print("******* world")
+    el.stop()
+
+
 el = loop.EventLoop()
-el.call_soon(lambda a: print(a), "hello world")
-el.call_later(1.0, lambda: print("timer 1"))
-el.call_later(2.0, lambda: print("timer 2"))
-el.call_later(2.5, lambda: el.stop())
+asyncio.set_event_loop(el)
+# el.call_soon(lambda a: print(a), "hello world")
+# el.call_later(1.0, lambda: print("timer 1"))
+# el.call_later(2.0, lambda: print("timer 2"))
+task = el.create_task(main())
+# el.call_later(5, lambda: el.stop())
+# el.run_until_complete(task)
 el.run_forever()
--- objc_asyncio/_loop.py
+++ objc_asyncio/_loop.py
@@ -4,31 +4,46 @@ import asyncio
 import heapq
 import socket
 import subprocess
+import sys
 
 from Cocoa import (
     CFAbsoluteTimeGetCurrent,
     CFRunLoopAddTimer,
     CFRunLoopGetCurrent,
     CFRunLoopPerformBlock,
+    CFRunLoopRemoveTimer,
     CFRunLoopRun,
     CFRunLoopStop,
     CFRunLoopTimerCreateWithHandler,
     CFRunLoopTimerGetNextFireDate,
+    CFRunLoopTimerInvalidate,
     CFRunLoopTimerSetNextFireDate,
+    CFRunLoopWakeUp,
     kCFRunLoopCommonModes,
 )
 
 _unset = object()
+_POSIX_TO_CFTIME = 978307200
 
 
-def _handle_callback(handle):
+def posix2cftime(posixtime):
+    """ Convert a POSIX timestamp to a CFAbsoluteTime timestamp """
+    return posixtime - _POSIX_TO_CFTIME
+
+
+def cftime2posix(cftime):
+    """ Convert a CFAbsoluteTime timestamp to a POSIX timestamp """
+    return cftime + _POSIX_TO_CFTIME
+
+
+def handle_callback(handle):
     if handle.cancelled():
         return
 
     handle._run()
 
 
-class EventLoop:
+class EventLoop(asyncio.AbstractEventLoop):
     """
     An asyncio eventloop that uses a Cocoa eventloop
 
@@ -60,12 +75,24 @@ class EventLoop:
 
         return future.result()
 
+    def _asyncgen_firstiter_hook(self, agen):
+        ...
+
+    def _asyncgen_finalizer_hook(self, agen):
+        ...
+
     def run_forever(self):
+        old_agen_hooks = sys.get_asyncgen_hooks()
+        sys.set_asyncgen_hooks(
+            firstiter=self._asyncgen_firstiter_hook,
+            finalizer=self._asyncgen_finalizer_hook,
+        )
         try:
             self._running = True
             CFRunLoopRun()
         finally:
             self._running = False
+            sys.set_asyncgen_hooks(*old_agen_hooks)
 
     def stop(self):
         CFRunLoopStop(self._loop)
@@ -86,25 +113,37 @@ class EventLoop:
         if self._closed:
             return
 
-        ...
+        if self._timer_q:
+            # Cancel timers?
+            ...
+
+        if self._timer is not None:
+            CFRunLoopRemoveTimer(self._loop, self._timer, kCFRunLoopCommonModes)
+            CFRunLoopTimerInvalidate(self._timer)
+            self._timer = None
+
+        self._closed = True
 
     def shutdown_asyncgens(self):
-        ...
+        raise NotImplementedError(1)
 
     # Scheduling callbacks
 
     def call_soon(self, callback, *args, context=None):
         handle = asyncio.Handle(callback, args, self, context)
         CFRunLoopPerformBlock(
-            self._loop, kCFRunLoopCommonModes, lambda: _handle_callback(handle)
+            self._loop, kCFRunLoopCommonModes, lambda: handle_callback(handle)
         )
+        CFRunLoopWakeUp(self._loop)
+
         return handle
 
     def call_soon_thread_safe(self, callback, *args, context=None):
         handle = asyncio.Handle(callback, args, self, context)
         CFRunLoopPerformBlock(
-            self._loop, kCFRunLoopCommonModes, lambda: _handle_callback(handle)
+            self._loop, kCFRunLoopCommonModes, lambda: handle_callback(handle)
         )
+        CFRunLoopWakeUp(self._loop)
         return handle
 
     # Scheduling delayed callbacks
@@ -115,28 +154,35 @@ class EventLoop:
     def _process_timer(self):
         while self._timer_q and self._timer_q[0].when() <= self.time():
             handle = heapq.heappop(self._timer_q)
-            _handle_callback(handle)
+            handle_callback(handle)
 
         if self._timer_q:
-            CFRunLoopTimerSetNextFireDate(self._timer, self._timer_q[0].when())
+            CFRunLoopTimerSetNextFireDate(
+                self._timer, posix2cftime(self._timer_q[0].when())
+            )
 
     def call_at(self, when, callback, *args, context=None):
+        cfwhen = posix2cftime(when)
         if self._timer is None:
             self._timer = CFRunLoopTimerCreateWithHandler(
-                None, when, 1000.0, 0, 0, lambda timer: self._process_timer()
+                None, cfwhen, 1000.0, 0, 0, lambda timer: self._process_timer()
             )
             CFRunLoopAddTimer(self._loop, self._timer, kCFRunLoopCommonModes)
 
         handle = asyncio.TimerHandle(when, callback, args, self, context=context)
         heapq.heappush(self._timer_q, handle)
 
-        if CFRunLoopTimerGetNextFireDate(self._timer) > self._timer_q[0].when():
-            CFRunLoopTimerSetNextFireDate(self._timer, self._timer_q[0].when())
+        if CFRunLoopTimerGetNextFireDate(self._timer) > posix2cftime(
+            self._timer_q[0].when()
+        ):
+            CFRunLoopTimerSetNextFireDate(
+                self._timer, posix2cftime(self._timer_q[0].when())
+            )
+
+        return handle
 
     def time(self):
-        # XXX: This is wrong, CFAbsoluteTimeGetCurrent has a different epoch
-        # than time.time.
-        return CFAbsoluteTimeGetCurrent()
+        return cftime2posix(CFAbsoluteTimeGetCurrent())
 
     # Creating Futures and Tasks
     def create_future(self):
@@ -178,7 +224,7 @@ class EventLoop:
         interleave=None
     ):
 
-        ...
+        raise NotImplementedError(2)
 
     async def create_datagram_endpoint(
         self,
@@ -194,7 +240,7 @@ class EventLoop:
         allow_broadcast=None,
         sock=None
     ):
-        ...
+        raise NotImplementedError(3)
 
     async def create_unix_connection(
         sefl,
@@ -206,7 +252,7 @@ class EventLoop:
         server_hostname=None,
         ssl_handshake_timeout=None
     ):
-        ...
+        raise NotImplementedError(4)
 
     # Creating network servers
     async def create_server(
@@ -225,7 +271,7 @@ class EventLoop:
         ssl_handshake_timeout=None,
         start_serving=True
     ):
-        ...
+        raise NotImplementedError(5)
 
     async def create_unix_server(
         self,
@@ -238,16 +284,16 @@ class EventLoop:
         ssl_handshake_timeout=None,
         start_serving=True
     ):
-        ...
+        raise NotImplementedError(6)
 
     async def connect_accepted_socket(
         self, protocol_factory, sock, *, ssl=None, ssl_handshake_timeout=None
     ):
-        ...
+        raise NotImplementedError(7)
 
     # Transfering files
     async def sendfile(transport, file, offset=0, count=None, *, fallback=True):
-        ...
+        raise NotImplementedError(8)
 
     # TLS Upgrade
     async def start_tls(
@@ -260,71 +306,71 @@ class EventLoop:
         server_hostname=None,
         ssl_handshake_timeout=None
     ):
-        ...
+        raise NotImplementedError(9)
 
     # Watching file descriptors
     async def add_reader(self, fd, callback, *args):
-        ...
+        raise NotImplementedError(10)
 
     async def remove_reader(self, fd):
-        ...
+        raise NotImplementedError(11)
 
     async def add_writer(self, fd, *args):
-        ...
+        raise NotImplementedError(12)
 
     async def remove_writer(self, fd):
-        ...
+        raise NotImplementedError(13)
 
     # Working with socket objects directly
 
     async def sock_recv(self, sock, nbytes):
-        ...
+        raise NotImplementedError(14)
 
     async def sock_recv_into(self, sock, buf):
-        ...
+        raise NotImplementedError(15)
 
     async def sock_sendall(self, sock, data):
-        ...
+        raise NotImplementedError(16)
 
     async def sock_connect(self, sock, address):
-        ...
+        raise NotImplementedError(17)
 
     async def sock_accept(self, sock):
-        ...
+        raise NotImplementedError(18)
 
     async def sock_sendfile(self, file, offset=0, count=None, *, fallback=True):
-        ...
+        raise NotImplementedError(19)
 
     # DNS
 
     async def getaddrinfo(
         self, host, port, *, family=0, type=0, proto=0, flags=0  # noqa: A002
     ):
-        ...
+        raise NotImplementedError(20)
 
     async def getnameinfo(self, sockaddr, flags=0):
-        ...
+        raise NotImplementedError(21)
 
     # Working with pipes
 
     async def connect_read_pipe(self, protocol_factory, pipe):
-        ...
+        raise NotImplementedError(22)
 
     async def connect_write_pipe(self, protocol_factory, pipe):
-        ...
+        raise NotImplementedError(23)
 
     # Unix signals
 
     def add_signal_handler(self, signum, callback, *args):
-        ...
+        raise NotImplementedError(24)
 
     def remove_singal_handler(self, signum):
-        ...
+        raise NotImplementedError(25)
 
     # Executing code in thread or process pools
 
     async def run_in_executor(self, executor, func, *args):
-        ...
+        raise NotImplementedError(26)
 
     def set_default_executor(self, executor):
         self._executor = executor
@@ -338,10 +384,10 @@ class EventLoop:
         return self._exeception_handler
 
     def default_exception_handler(self, context):
-        ...
+        print("Exception", context)
 
     def call_exception_handler(self, context):
-        ...
+        print("call_exception_handler", context)
 
     # Enabling debug mode
 
@@ -362,7 +408,7 @@ class EventLoop:
         stderr=subprocess.PIPE,
         **kwargs
     ):
-        ...
+        raise NotImplementedError(27)
 
     async def subprocess_shell(
         self,
@@ -374,4 +420,11 @@ class EventLoop:
         stderr=subprocess.PIPE,
         **kwargs
     ):
-        ...
+        raise NotImplementedError(28)
+
+    #
+    #
+
+    def _timer_handle_cancelled(self, handle):
+        if handle._scheduled:
+            self._timer_cancelled_count += 1
