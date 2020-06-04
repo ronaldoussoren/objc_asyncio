e516cff19686708d94e1cb03009eaf3fd7ef4003
Ronald Oussoren <ronaldoussoren@mac.com>
2020-06-04 22:31:52 +0200

    Export KQueueChildWatcher

--- objc_asyncio/__init__.py
+++ objc_asyncio/__init__.py
@@ -8,3 +8,4 @@ __version__ = "0.1"
 from ._helpers import IBAction  # noqa: F401
 from ._loop import PyObjCEventLoop  # noqa: F401
 from ._loop_policy import PyObjCEventLoopPolicy  # noqa: F401
+from ._subprocess import KQueueChildWatcher  # noqa: F401
