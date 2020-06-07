f27d7323f18de3fde00c15798bf7b4a39215b753
Ronald Oussoren <ronaldoussoren@mac.com>
2020-06-07 15:59:16 +0200

    Test cleanup

--- testsuite/test_exceptionhandler.py
+++ testsuite/test_exceptionhandler.py
@@ -1,8 +1,4 @@
-import io
-import logging
 import traceback
-
-from objc_asyncio._log import logger
 
 from . import utils
 
@@ -39,15 +35,11 @@ class TestExceptionHandler(utils.TestCase):
         self.loop.call_exception_handler(context)
         self.assertEqual(handled, [(self.loop, context)])
 
-        stream = io.StringIO()
-        handler = logging.StreamHandler(stream)
-        logger.addHandler(handler)
-        self.addCleanup(logger.removeHandler, handler)
-
-        # Make sure exceptions are swallowed
-        handled = None
-        self.loop.call_exception_handler(context)
-        self.assertIs(handled, None)
+        with utils.captured_log() as stream:
+            # Make sure exceptions are swallowed
+            handled = None
+            self.loop.call_exception_handler(context)
+            self.assertIs(handled, None)
 
         contents = stream.getvalue()
 
@@ -55,13 +47,9 @@ class TestExceptionHandler(utils.TestCase):
         self.assertIn("AttributeError", contents)
 
     def test_default_handler(self):
-        stream = io.StringIO()
-        handler = logging.StreamHandler(stream)
-        logger.addHandler(handler)
-        self.addCleanup(logger.removeHandler, handler)
-
-        context = {"attribute": "value", "dogs": "cats"}
-        self.loop.call_exception_handler(context)
+        with utils.captured_log() as stream:
+            context = {"attribute": "value", "dogs": "cats"}
+            self.loop.call_exception_handler(context)
 
         contents = stream.getvalue()
 
@@ -72,100 +60,59 @@ class TestExceptionHandler(utils.TestCase):
         self.assertIn("cats", contents)
 
     def test_default_handler_with_traceback(self):
-        stream = io.StringIO()
-        handler = logging.StreamHandler(stream)
-        logger.addHandler(handler)
-        self.addCleanup(logger.removeHandler, handler)
+        with utils.captured_log() as stream:
+            for key, needle in [
+                ("source_traceback", "Object"),
+                ("handle_traceback", "Handle"),
+            ]:
 
-        for key, needle in [
-            ("source_traceback", "Object"),
-            ("handle_traceback", "Handle"),
-        ]:
+                with self.subTest(key=key):
 
-            with self.subTest(key=key):
+                    stream.seek(0)
+                    stream.truncate()
 
-                stream.seek(0)
-                stream.truncate()
+                    tb = traceback.extract_stack()
 
-                tb = traceback.extract_stack()
-
-                context = {"attribute": "value", key: tb}
-                self.loop.call_exception_handler(context)
-
-                contents = stream.getvalue()
-
-                self.assertIn("Unhandled exception in event loop", contents)
-                self.assertIn("attribute", contents)
-                self.assertIn("value", contents)
-
-                self.assertIn(f"{needle} created at (most", contents)
-
-    def test_default_handler_with_handle_traceback(self):
-        stream = io.StringIO()
-        handler = logging.StreamHandler(stream)
-        logger.addHandler(handler)
-        self.addCleanup(logger.removeHandler, handler)
-
-        tb = traceback.extract_stack()
-
-        context = {"attribute": "value"}
-        self.loop._current_handle = Attr()
-        self.loop._current_handle._source_traceback = tb
-
-        self.loop.call_exception_handler(context)
-
-        contents = stream.getvalue()
-
-        self.assertIn("Unhandled exception in event loop", contents)
-        self.assertIn("attribute", contents)
-        self.assertIn("value", contents)
-
-        self.assertIn(f"Handle created at (most", contents)
-
-    def test_exit_in_exception_handler(self):
-        stream = io.StringIO()
-        handler = logging.StreamHandler(stream)
-        logger.addHandler(handler)
-        self.addCleanup(logger.removeHandler, handler)
-
-        def handler(loop, context):
-            raise exception
-
-        self.loop.set_exception_handler(handler)
-
-        for exception in (SystemExit, KeyboardInterrupt):
-            with self.subTest(exception=exception):
-                stream.seek(0)
-                stream.truncate()
-
-                async def main():
-                    context = {"a": "b"}
+                    context = {"attribute": "value", key: tb}
                     self.loop.call_exception_handler(context)
 
-                with self.assertRaises(exception):
-                    self.loop.run_until_complete(main())
+                    contents = stream.getvalue()
 
-                self.assertEqual(stream.getvalue(), "")
+                    self.assertIn("Unhandled exception in event loop", contents)
+                    self.assertIn("attribute", contents)
+                    self.assertIn("value", contents)
 
-    def test_exit_in_default_handler(self):
-        stream = io.StringIO()
-        handler = logging.StreamHandler(stream)
-        logger.addHandler(handler)
-        self.addCleanup(logger.removeHandler, handler)
+                    self.assertIn(f"{needle} created at (most", contents)
 
-        def handler(loop, context):
-            raise ValueError("dummy")
+    def test_default_handler_with_handle_traceback(self):
+        with utils.captured_log() as stream:
 
-        def default_handler(context):
-            raise exception
+            tb = traceback.extract_stack()
 
-        self.loop.default_exception_handler = default_handler
+            context = {"attribute": "value"}
+            self.loop._current_handle = Attr()
+            self.loop._current_handle._source_traceback = tb
 
-        for handler in (None, handler):
+            self.loop.call_exception_handler(context)
+
+            contents = stream.getvalue()
+
+            self.assertIn("Unhandled exception in event loop", contents)
+            self.assertIn("attribute", contents)
+            self.assertIn("value", contents)
+
+            self.assertIn("Handle created at (most", contents)
+
+    def test_exit_in_exception_handler(self):
+        with utils.captured_log() as stream:
+
+            def handler(loop, context):
+                raise exception
+
             self.loop.set_exception_handler(handler)
 
             for exception in (SystemExit, KeyboardInterrupt):
-                with self.subTest(handler=handler, exception=exception):
+                with self.subTest(exception=exception):
                     stream.seek(0)
                     stream.truncate()
 
@@ -178,39 +125,65 @@ class TestExceptionHandler(utils.TestCase):
 
                     self.assertEqual(stream.getvalue(), "")
 
+    def test_exit_in_default_handler(self):
+        with utils.captured_log() as stream:
+
+            def handler(loop, context):
+                raise ValueError("dummy")
+
+            def default_handler(context):
+                raise exception
+
+            self.loop.default_exception_handler = default_handler
+
+            for handler in (None, handler):
+                self.loop.set_exception_handler(handler)
+
+                for exception in (SystemExit, KeyboardInterrupt):
+                    with self.subTest(handler=handler, exception=exception):
+                        stream.seek(0)
+                        stream.truncate()
+
+                        async def main():
+                            context = {"a": "b"}
+                            self.loop.call_exception_handler(context)
+
+                        with self.assertRaises(exception):
+                            self.loop.run_until_complete(main())
+
+                        self.assertEqual(stream.getvalue(), "")
+
     def test_error_in_default_handler(self):
-        class MyException(Exception):
-            pass
+        with utils.captured_log() as stream:
 
-        stream = io.StringIO()
-        handler = logging.StreamHandler(stream)
-        logger.addHandler(handler)
-        self.addCleanup(logger.removeHandler, handler)
+            class MyException(Exception):
+                pass
 
-        def handler(loop, context):
-            raise ValueError("dummy")
+            def handler(loop, context):
+                raise ValueError("dummy")
 
-        def default_handler(context):
-            raise exception
+            def default_handler(context):
+                raise exception
 
-        self.loop.default_exception_handler = default_handler
+            self.loop.default_exception_handler = default_handler
 
-        for handler in (None, handler):
-            self.loop.set_exception_handler(handler)
+            for handler in (None, handler):
+                self.loop.set_exception_handler(handler)
 
-            for exception in (MyException,):
-                with self.subTest(handler=handler, exception=exception):
-                    stream.seek(0)
-                    stream.truncate()
+                for exception in (MyException,):
+                    with self.subTest(handler=handler, exception=exception):
+                        stream.seek(0)
+                        stream.truncate()
 
-                    context = {"a": "b"}
-                    self.loop.call_exception_handler(context)
+                        context = {"a": "b"}
+                        self.loop.call_exception_handler(context)
 
-                    self.assertIn(
-                        "Exception in default exception handler", stream.getvalue()
-                    )
-                    if handler is not None:
                         self.assertIn(
-                            "while handling an unexpected error in custom exception handler",
-                            stream.getvalue(),
+                            "Exception in default exception handler", stream.getvalue()
                         )
+                        if handler is not None:
+                            self.assertIn(
+                                "while handling an unexpected error in custom "
+                                "exception handler",
+                                stream.getvalue(),
+                            )
